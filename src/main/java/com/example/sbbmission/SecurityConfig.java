63d20baa9b81a15ed6b18d92c43d4cca7c39a2ef
SeoungminKwon <tjdalsrnjs10@gmail.com>
2023-11-27 13:04:03 +0900

    3-5 spring security

---
+++ src/main/java/com/example/sbbmission/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.example.sbbmission;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration //스프링 환경설정파일을 명시해주는 어노테이션
+@EnableWebSecurity //모든 요청 URL이 스프링 시큐리티의제어를 받도록 만드는 어노테이션
+// -> 사용시 내부적으로 SpringSecurityFilterChain이 동작 -> URL필터가 적용
+public class SecurityConfig {
+    @Bean //시큐리티 세부 설정은 SecurityFilterChain 빈을 생성하여 설정 가능
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        //아래의 문장은 모든 인증되지않은 요청을 허락한다는 의미
+        http
+                .authorizeHttpRequests((authorizeRequests) -> authorizeRequests
+                        .requestMatchers(new AntPathRequestMatcher("/**")).permitAll()
+                ).csrf((csrf) -> csrf
+                        .ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**"))
+                ).headers((headers) -> headers
+                        .addHeaderWriter(new XFrameOptionsHeaderWriter(
+                                XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN
+                        )));
+
+
+        return http.build();
+    }
+}
