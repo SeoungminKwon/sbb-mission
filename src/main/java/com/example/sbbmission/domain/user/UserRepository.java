58b52bb178e8c24b175d237d956ff01c0a390c6d
SeoungminKwon <tjdalsrnjs10@gmail.com>
2023-11-27 21:08:37 +0900

    3-7 login and logout

--- src/main/java/com/example/sbbmission/domain/user/UserRepository.java
+++ src/main/java/com/example/sbbmission/domain/user/UserRepository.java
@@ -1,6 +1,10 @@
 package com.example.sbbmission.domain.user;
 
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository< SiteUser, Long > {
+    Optional< SiteUser > findByusername(String username);
 }
